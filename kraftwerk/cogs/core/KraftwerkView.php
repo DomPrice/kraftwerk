de7c16bec04993131c5f9904f587598ff1b7de90
Dom Price <info@domprice.com>
2013-07-28 17:33:45 -0600

    Update KraftwerkView.php
    Stripped View Class in core of any superfluous code. Hasn't been tested yet.

--- kraftwerk/cogs/core/KraftwerkView.php
+++ kraftwerk/cogs/core/KraftwerkView.php
@@ -11,47 +11,8 @@
 */
 class KraftwerkView {
 
-	// CLASS VARIABLES
-	var $CLASSVERSION 		= '0.0.3';
-	var $CLASSDATE			= '08/08/2010';
-	var $CLASSAUTHORS		= 'Dom Price';
-	var $CONFIG_LAYOUT_DIR 	= '';
 
-	// CONFIG STRUCT
-	var $CONFIG 			= array();	
-	var $CONSTANTS 			= array();	
-	
-	// OTHER STRUCTS
-	var $VIEWS_LOADED 	= array();
-
-	// LAYOUT DATA FOR LATER PARSING
-	var $USE_LAYOUT			= "";
-	var $LAYOUT_DATA		= "";
-
-	// ESTABLISH NEW DATABASE CONNECTOR FOR THE LOGIN SYSTEM TO USE
-	protected $viewConnector = "";
-
-	// CREATE NEW UTILITY CLASS
-	protected $utilities = "";
-
-	/* 
-		CONSTRUTOR
-	*/
-	public function __construct($loaded_layouts=array()) {
-
-		// ESTABLISH LOGIN CONNECTOR, WE NEED TO GRAB THE MASTER DATABASE DATA FROM THE $GLOBAL ENVIRONMENT
-		$login_user 	= $GLOBALS['kraftwerk']->CONFIG["content_database_username"];
-		$login_pass 	= $GLOBALS['kraftwerk']->CONFIG["content_database_password"]; 
-		$login_host 	= $GLOBALS['kraftwerk']->CONFIG["content_database_server"];
-		$login_schema 	= $GLOBALS['kraftwerk']->CONFIG["content_database_schema"];
-		$this->viewConnector = new DatabaseConnector($login_host, $login_user, $login_pass, $login_schema);
-
-		// INITIALIZE UTILITY CLASS FOR COMMON FUNCTIONS
-		$this->utilities = new KraftwerkUtility();
-
-		// LOAD THE VIEWS 
-		$this->loadViews($loaded_layouts);
-
+	public function __construct() {
 	}
 
 	/* 
