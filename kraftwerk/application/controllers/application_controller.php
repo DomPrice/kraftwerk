4decc9ae9920254f82d968d970692f1d99f9cfe2
unknown <Dom Price@Ulan.(none)>
2014-03-12 00:46:52 -0400

    added application controller shell

---
+++ kraftwerk/application/controllers/application_controller.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+	This is the application controller for this kraftwerk app
+	this gets loaded before everythimg else
+*/
+
+class ApplicationController extends KraftwerkController {
+
+
+}
+
+?>
