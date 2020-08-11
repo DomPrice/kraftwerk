fa4c382b9d70ab8233657d06ff6b2bba4fa5de2e
Dom Price <info@domprice.com>
2020-08-11 14:43:45 -0400

    removed E_Warning from init.php

--- .kraftwerk/run/init.php
+++ .kraftwerk/run/init.php
@@ -23,7 +23,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 //error_reporting(E_ALL);
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
+error_reporting(E_ERROR | E_PARSE);
 
 // LOAD CONSTANTS
 include_once(dirname(__FILE__) . '/dep/constants.php');
